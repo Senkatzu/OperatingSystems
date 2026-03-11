323957d1c4a17f7fb10b789020f6f1379370b0be
Senkatzu <134923303+Senkatzu@users.noreply.github.com>
2026-03-12 03:04:52 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Supabase Configuration
+// This file contains sensitive credentials - DO NOT commit to GitHub!
+const SUPABASE_CONFIG = {
+    URL: 'https://xpxfydxxszqzokbeaaip.supabase.co',
+    ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhweGZ5ZHh4c3pxem9rYmVhYWlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyNDUxNTMsImV4cCI6MjA4ODgyMTE1M30.7XDSLYR_igb-LxuMWVGfZRnc6ahoPL9PRCWQRwN9yfA'
+};
